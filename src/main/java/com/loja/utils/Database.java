b488b2726b0c5b8fc4a6937d5d20c0b3b9489ab2
JUNIOR ALVES <junior_a.lima@hotmail.com>
2024-08-20 11:37:23 -0300

    primeiro commit

--- src/main/java/com/loja/utils/Database.java
+++ src/main/java/com/loja/utils/Database.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class Database {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/db_projeto";
-    private static final String USER = "postgres"; // Nome do Usuario do Banco
-    private static final String PASSWORD = "******"; // Senha do banco do usuario.
+    private static final String URL = "jdbc:postgresql://localhost:5432/********"; // Substitua as estralas pelo nome do banco de dados.
+    private static final String USER = "******"; // Substitua as estralas pelo Nome do Usuario do Banco.
+    private static final String PASSWORD = "*******"; // Substitua as estralas pela Senha do banco do usuario.
 
     static {
         try {
