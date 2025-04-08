e5e8a8e1f94d5e7f3ffd1af601933d30e7024505
JUNIOR ALVES <134221922+JRALVESLIMA@users.noreply.github.com>
2025-04-08 08:30:45 -0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,12 +7,10 @@ module com.loja.emporio {
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
 
-    // Abre os pacotes necessários para o JavaFX
     opens com.loja to javafx.fxml;
     opens com.loja.controllers to javafx.fxml;
     opens com.loja.models to javafx.base;
 
-    // Exporta os pacotes conforme necessário
     exports com.loja;
     exports com.loja.controllers;
     exports com.loja.models;
